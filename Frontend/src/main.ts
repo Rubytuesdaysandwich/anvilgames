5b3991b46f9023c83e844d6691cbf67e8562418a
Rubytuesdaysandwich <88746844+Rubytuesdaysandwich@users.noreply.github.com>
2024-01-04 15:33:33 -0700

    update worked on currentgameview. making dyn canvas

--- Frontend/src/main.ts
+++ Frontend/src/main.ts
@@ -1,10 +1,11 @@
 //This is Vue.js root instance. global to all other pages
 import { createApp } from "vue";
+
 import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store"
-
+import { defineCustomElements as defineIonPhaser } from '@ion-phaser/core/loader';
 //add phaser here?
 // Import fontawesome 
 /* import the fontawesome core */
@@ -14,8 +15,18 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 /* import specific icons */
 import { fas } from '@fortawesome/free-solid-svg-icons'
 
+
+
 library.add(fas)
 const app = createApp(App)
+app.config.globalProperties.$config = {
+    //when the project is hosted this should be on so that users don't see the production tips later
+    // productionTip: false,
+    //ignoring the elements starting with ion because they are not part of vue we use this to avoid error being given
+    ignoredElements: [/ion-\w*/],
+
+}
+defineIonPhaser(window);
 app.component('font-awesome-icon', FontAwesomeIcon).use(store).use(router).mount("#app");
 
 
---
+++ Frontend/src/views/CurrentGameView.vue
@@ -0,0 +1,76 @@
+<template>
+    <!-- if the game is initialized the game will show -->
+<div v-if="intialize">
+<canvas ref="gameCanvas">
+
+</canvas>
+</div>
+</template>
+
+
+
+
+<script>
+import * as Phaser from "phaser"
+import { onMounted, ref } from "vue";
+
+
+export default{
+    name:"CurrentGameView",
+
+
+    data(){
+        return{
+            initialize:false,
+            game:null,
+        };
+
+    },
+    methods:{
+        // Example usage for two different games with different physics engines we can call this function when we config the game engine for different types of games
+// const gameConfig1 = createGameConfig(800, 600, ExampleScene1, 200, 'arcade');
+// const gameConfig2 = createGameConfig(1200, 800, ExampleScene2, 300, 'p2');
+createGameConfig(width,height,scene,gravity,physcicsType){
+    return{
+
+        type:Phaser.AUTO,
+        width:width,
+        height:height,
+        scene:scene,
+        physcics:{
+            default:physcicsType,
+            [physcicsType]:{
+                gravity:{y:gravity}
+            }
+        }
+    }
+},
+
+initializeGame (){
+this.initialize = true;
+}
+
+    },
+    onMounted(){
+        this.initializeGame();//run when the CurrentGame component is loaded. it will be the only loaded component on screen
+    }
+     
+        //create a new phaser game instance
+        
+        
+
+        
+        // Ensure canvasElement is a valid HTMLCanvasElement
+        
+        
+        // /**
+        //  * Creates the first game
+        //  * @param {HTMLCanvasElement} canvasElement 
+        //  */
+        // function Game1(canvasElement) {
+            //     const game = new Phaser.Game({
+                //     canvas: canvasElement
+                // });
+}
+
+</script>
